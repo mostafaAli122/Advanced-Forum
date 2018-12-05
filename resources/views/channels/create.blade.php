43e7731b69931c65742d888d7d80972c40821cd5
mostafaAli122 <mostafaali122@outlook.com>
2018-12-05 14:00:39 +0200

    fix error

--- resources/views/channels/create.blade.php
+++ resources/views/channels/create.blade.php
@@ -1,31 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Create a New Channel </div>
+    <div class="card">
+        <div class="card-header">Create a New Channel </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form action="{{ route('channels.store')}}" method="post">
-                        {{csrf_field()}}
-                        <div class="form-group">
-                            <input type="text" name="channel" class="form-control">
-                        </div>
-                        <div class="form-group text-center">
-                            <button class="btn btn-success" type="submit">Save Channel </button>
-                        </div>
-                    </form>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <form action="{{ route('channels.store')}}" method="post">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <input type="text" name="channel" class="form-control">
+                </div>
+                <div class="form-group text-center">
+                    <button class="btn btn-success" type="submit">Save Channel </button>
+                </div>
+            </form>
         </div>
     </div>
-</div>
+        
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,19 +73,24 @@
         </nav>
 
         <div class="container">
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Channels</div>
-                    <div class="panel-body">
-                        <ul class="list-group">
-                            @foreach($channels as $channel)
-                                <li class="list-group-item">{{$channel->title}}</li>
-                            @endforeach
-                        </ul>
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">Channels</div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                @foreach($channels as $channel)
+                                    <li class="list-group-item">{{$channel->title}}</li>
+                                @endforeach
+                            </ul>
+                        </div>
                     </div>
                 </div>
+
+                <div class="col-md-8">
+                @yield('content')
+                </div>
             </div>
-            @yield('content')
         </div>
     </div>
 </body>
