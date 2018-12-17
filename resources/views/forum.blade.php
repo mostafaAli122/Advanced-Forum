cfffe3bfd4847e7c3e4126ab26e6c789686b8084
mostafaAli122 <mostafaali122@outlook.com>
2018-12-17 12:14:33 +0200

    mark discussion as open or colsed if it has a best answer

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -7,7 +7,12 @@
             <div class="card-header">
                 <img src="{{ $d->user->avatar}}" alt="" width="40px" hight="40px" >&nbsp;&nbsp;&nbsp;
                 <span>{{ $d->user->name}}, <b>{{$d->created_at->diffForHumans()}} </b> </span>
-                <a href="{{ route('discussion',['slug'=>$d->slug])}}" class="btn btn-default float-right">View</a>
+                @if($d->hasBestAnswer())
+                    <span class="btn btn float-rigth bnt-success btn-xs">Closed</span>
+                @else
+                    <span class="btn btn float-rigth bnt-danger btn-xs">Open</span>
+                @endif
+                <a href="{{ route('discussion',['slug'=>$d->slug])}}" class="btn btn-default btn-xs float-right">View</a>
             </div>
 
             <div class="card-body">
